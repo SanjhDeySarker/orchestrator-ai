ed96cad4917b1ceb0b581a476f715526dc49b743
Sanjh Dey Sarker <sanjhdeysarker@gmail.com>
2025-09-06 22:09:10 +0530

    First Commit

---
+++ src/agents/types.ts
@@ -0,0 +1,25 @@
+import type {Channel, StreamChat, User} from "stream-chat"
+
+
+
+export interface AIAgent {
+    user?: User
+    channel: Channel
+    chatClent: StreamChat
+    getLastInteraction: () => number
+    init: () => Promise<void>;
+    dispose: () => Promise<void>;
+}
+
+export enum AgentPlatform {
+    OPENAI = "openai",
+    WRITING_ASSISTANT="writing_assistant"
+}
+
+export interface WritingMessage {
+    custome?: {
+        suggestions?: string[]
+        writingTask?: string
+        messageType?: "user_input" | "ai_response" | "system_message";
+    };
+}
